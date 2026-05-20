aa936e1741abca5954dc0c7a193240f3f426c0cf
aerobis <185216831+aerobis@users.noreply.github.com>
2026-05-20 21:33:44 +0545

    Made the select-section.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 //Y7L3P4JECFHQUZ3526P4V54U8
 
 import './style.css';
+import {selectSectionMaker} from "./select-section.js";
 
-
+document.addEventListener('DOMContentLoaded', ()=>{
+    selectSectionMaker();
+});
--- src/select-section.js
+++ src/select-section.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
-    
-});
+export function selectSectionMaker(){
+    let container = document.querySelector(".main-content");
+
+    let selectSection = document.createElement('div');
+    selectSection.classList.add('select-section');
+
+    let selectSectionContent = document.createElement('div');
+    selectSectionContent.classList.add('select-section-content');
+
+    let label = document.createElement('p');
+    label.classList.add('select-section-text');
+    label.textContent = "Select your location: ";
+    label.setAttribute('for', 'location-input');
+
+    let locationInput = document.createElement('input');
+    locationInput.id = 'location-input';
+    locationInput.setAttribute('placeholder', 'location...');
+
+    let submitBtn = document.createElement('button');
+    submitBtn.id = 'submit-button';
+    submitBtn.textContent = "Search";
+
+    selectSectionContent.appendChild(label);
+    selectSectionContent.appendChild(locationInput);
+    selectSectionContent.appendChild(submitBtn);
+    selectSection.appendChild(selectSectionContent);
+    container.appendChild(selectSection);
+}
