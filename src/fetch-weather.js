621502fe4a716fb0bde5c9b3a6f3078c88866ff0
aerobis <185216831+aerobis@users.noreply.github.com>
2026-06-02 20:50:07 +0545

    Improved the weather fetching logic.

--- src/fetch-weather.js
+++ src/fetch-weather.js
@@ -2,14 +2,24 @@
 
 export async function fetchWeather(location){
     try{
-        let request = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=Y7L3P4JECFHQUZ3526P4V54U8`);
-        let result = await request.json();
+        const request = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=Y7L3P4JECFHQUZ3526P4V54U8`);
+        const result = await request.json();
+		const days = result.days.slice(0,7);
+        let processedData = {};
 
-        // for(let i=0; i<7; i++){
-        
-        // }
+        const forecast = days.map(day => {
+            return {
+                date: day.datetime,
+                temp: day.temp,
+                conditions: day.conditions
+            };
+        });
 
-        console.log(result);
+		return{
+            forecast: forecast
+        }
+	
+        console.log(forecast);
     }catch(error){
         console.log(error);
     }
